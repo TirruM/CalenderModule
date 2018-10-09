9bc39b293db3b40dbb8790629b7922879ee9d745
ravi9168 <ravisavarapu@gmail.com>
2018-10-09 17:47:21 +0530

    commit

--- src/app/halfyearly/halfyearly.component.ts
+++ src/app/halfyearly/halfyearly.component.ts
@@ -9,9 +9,9 @@ import { QuarterlyModel } from '../models/quarterlyModel';
   styleUrls: ['./halfyearly.component.css']
 })
 export class HalfyearlyComponent implements OnInit {
-  selectedCheckBox: number = 0;
-  firstCheckBox: boolean = true;
-  secondCheckBox: boolean = true;
+  selectedCheckBox = 0;
+  firstCheckBox = true;
+  secondCheckBox = true;
   selectOption: any[] = [
     { id: 1, name: 'First', value: '1', checked: false },
     { id: 2, name: 'Second', value: '2', checked: false }
@@ -30,7 +30,7 @@ export class HalfyearlyComponent implements OnInit {
   public novDate: Date;
   public decDate: Date;
 
-  public selectYearHalf: any;
+  public selectYear: any;
   public startYearHalf: string;
   public yearHalf: string;
   public mainErrorMsg = '';
@@ -42,14 +42,9 @@ export class HalfyearlyComponent implements OnInit {
 
   @Output() public dateChanged = new EventEmitter();
 
-
-  public startAt = new Date(2018, 3, 18, 10, 30);
-
-  // Min moment: April 12 2018, 10:30
-  public min = new Date(2018, 3, 12, 10, 30);
-
-  // Max moment: April 25 2018, 20:30
-  public max = new Date(2018, 3, 25, 20, 30);
+  public startAt = null;
+  public min = null;
+  public max = null;
 
   constructor(public datePipe: DatePipe) {
     this.quarterlyModelArray.quarterlyModel = [];
@@ -61,13 +56,21 @@ export class HalfyearlyComponent implements OnInit {
 
 
   checkBoxChangeEvent(event) {
-    var val = event.target.value;
-    var index = this.selectOption.findIndex((ds: any) => {
-      return ds.id + "" === event.target.id;
+    if (this.startYearHalf === undefined) {
+      this.mainErrorMsg = 'Please select Year';
+      this.dateValidationFlagHalf = true;
+      this.firstCheckBox = false;
+      this.secondCheckBox = false;
+    } else {
+
+      this.dateValidationFlagHalf = false;
+    }
+
+    let val = event.target.value;
+    let index = this.selectOption.findIndex((ds: any) => {
+      return ds.id + '' === event.target.id;
     });
     this.selectedCheckBox = index;
-    // console.log(val);
-    // console.log(event.target.checked);
     if (val === '1' && event.target.checked) {
       this.firstCheckBox = false;
     } else if (val === '1' && !event.target.checked) {
@@ -88,24 +91,21 @@ export class HalfyearlyComponent implements OnInit {
   }
 
   public getMonthFirstLastDay(year, month): any {
-    //console.log("month" + month);
-    var monthStartDay = new Date(year, month, 1);
-    var monthEndDay = new Date(year, month + 1, 0);
-    var min = this.datePipe.transform(monthStartDay, 'dd');
-    var max = this.datePipe.transform(monthEndDay, 'dd');
+    let monthStartDay = new Date(year, month, 1);
+    let monthEndDay = new Date(year, month + 1, 0);
+    let min = this.datePipe.transform(monthStartDay, 'dd');
+    let max = this.datePipe.transform(monthEndDay, 'dd');
 
     this.min = new Date(this.selectYear, month, Number.parseInt(min));
     this.max = new Date(this.selectYear, month, Number.parseInt(max));
-    //console.log("min::" + min + "max:::" + max);
-  }
+     }
 
   getDateTimeEvent(val) {
-    this.selectYear = this.startAt.getFullYear();
+   // this.selectYear = this.startAt.getFullYear();
     this.getMonthFirstLastDay(this.selectYear, val);
   }
 
   handleChange(event) {
-    //console.log("Half handle change");
     this.dateChanged.emit(this.quarterlyModelArray);
   }
 
@@ -117,13 +117,13 @@ export class HalfyearlyComponent implements OnInit {
     if (this.secondHalfyearly.length > 0) {
       this.quarterlyModelArray.quarterlyModel.push(this.secondHalfyearly[0]);
     }
-    //console.log("halfYearlyModelArray" + JSON.stringify(this.quaterlyModelArray));
     this.handleChange(event);
   }
 
 
   handleSelectYearHalf(year: string) {
     this.yearHalf = year;
+    this.selectYear = year;
     this.startYearHalf = year;
     this.dateValidationFlagHalf = false;
   }
@@ -131,7 +131,7 @@ export class HalfyearlyComponent implements OnInit {
 
 
   clickMonthEvent(val) {
-    if (val == 0) {
+    if (val === 0) {
       this.getDateTimeEvent(val);
       this.febDate = null;
       this.marchDate = null;
@@ -141,14 +141,14 @@ export class HalfyearlyComponent implements OnInit {
 
       this.firstHalfyearly = [];
       let calenderModel1 = new CalenderModel();
-      var jan = this.datePipe.transform(this.janDate, 'yyyy-MM-dd');
+      let jan = this.datePipe.transform(this.janDate, 'yyyy-MM-dd');
       calenderModel1.start_date = jan;
       calenderModel1.end_date = jan;
       this.firstHalfyearly.push(calenderModel1);
       this.prepareQuaterlyObj();
 
 
-    } else if (val == 1) {
+    } else if (val === 1) {
       this.getDateTimeEvent(val);
       this.janDate = null;
       this.marchDate = null;
@@ -158,13 +158,13 @@ export class HalfyearlyComponent implements OnInit {
 
       this.firstHalfyearly = [];
       let calenderModel1 = new CalenderModel();
-      var jan = this.datePipe.transform(this.febDate, 'yyyy-MM-dd');
+      let jan = this.datePipe.transform(this.febDate, 'yyyy-MM-dd');
       calenderModel1.start_date = jan;
       calenderModel1.end_date = jan;
       this.firstHalfyearly.push(calenderModel1);
       this.prepareQuaterlyObj();
 
-    } else if (val == 2) {
+    } else if (val === 2) {
       this.getDateTimeEvent(val);
       this.janDate = null;
       this.febDate = null;
@@ -174,13 +174,13 @@ export class HalfyearlyComponent implements OnInit {
 
       this.firstHalfyearly = [];
       let calenderModel1 = new CalenderModel();
-      var jan = this.datePipe.transform(this.marchDate, 'yyyy-MM-dd');
+      let jan = this.datePipe.transform(this.marchDate, 'yyyy-MM-dd');
       calenderModel1.start_date = jan;
       calenderModel1.end_date = jan;
       this.firstHalfyearly.push(calenderModel1);
       this.prepareQuaterlyObj();
 
-    } else if (val == 3) {
+    } else if (val === 3) {
       this.getDateTimeEvent(val);
       this.janDate = null;
       this.febDate = null;
@@ -190,13 +190,13 @@ export class HalfyearlyComponent implements OnInit {
 
       this.firstHalfyearly = [];
       let calenderModel1 = new CalenderModel();
-      var jan = this.datePipe.transform(this.aprDate, 'yyyy-MM-dd');
+      let jan = this.datePipe.transform(this.aprDate, 'yyyy-MM-dd');
       calenderModel1.start_date = jan;
       calenderModel1.end_date = jan;
       this.firstHalfyearly.push(calenderModel1);
       this.prepareQuaterlyObj();
 
-    } else if (val == 4) {
+    } else if (val === 4) {
       this.getDateTimeEvent(val);
       this.janDate = null;
       this.febDate = null;
@@ -206,13 +206,13 @@ export class HalfyearlyComponent implements OnInit {
 
       this.firstHalfyearly = [];
       let calenderModel1 = new CalenderModel();
-      var jan = this.datePipe.transform(this.mayDate, 'yyyy-MM-dd');
+      let jan = this.datePipe.transform(this.mayDate, 'yyyy-MM-dd');
       calenderModel1.start_date = jan;
       calenderModel1.end_date = jan;
       this.firstHalfyearly.push(calenderModel1);
       this.prepareQuaterlyObj();
 
-    } else if (val == 5) {
+    } else if (val === 5) {
       this.getDateTimeEvent(val);
       this.janDate = null;
       this.febDate = null;
@@ -222,13 +222,13 @@ export class HalfyearlyComponent implements OnInit {
 
       this.firstHalfyearly = [];
       let calenderModel1 = new CalenderModel();
-      var jan = this.datePipe.transform(this.juneDate, 'yyyy-MM-dd');
+      let jan = this.datePipe.transform(this.juneDate, 'yyyy-MM-dd');
       calenderModel1.start_date = jan;
       calenderModel1.end_date = jan;
       this.firstHalfyearly.push(calenderModel1);
       this.prepareQuaterlyObj();
 
-    } else if (val == 6) {
+    } else if (val === 6) {
       this.getDateTimeEvent(val);
       this.augDate = null;
       this.sepDate = null;
@@ -238,13 +238,13 @@ export class HalfyearlyComponent implements OnInit {
 
       this.secondHalfyearly = [];
       let calenderModel1 = new CalenderModel();
-      var jan = this.datePipe.transform(this.julDate, 'yyyy-MM-dd');
+      let jan = this.datePipe.transform(this.julDate, 'yyyy-MM-dd');
       calenderModel1.start_date = jan;
       calenderModel1.end_date = jan;
       this.secondHalfyearly.push(calenderModel1);
       this.prepareQuaterlyObj();
 
-    } else if (val == 7) {
+    } else if (val === 7) {
       this.getDateTimeEvent(val);
       this.julDate = null;
       this.sepDate = null;
@@ -254,13 +254,13 @@ export class HalfyearlyComponent implements OnInit {
 
       this.secondHalfyearly = [];
       let calenderModel1 = new CalenderModel();
-      var jan = this.datePipe.transform(this.augDate, 'yyyy-MM-dd');
+      let jan = this.datePipe.transform(this.augDate, 'yyyy-MM-dd');
       calenderModel1.start_date = jan;
       calenderModel1.end_date = jan;
       this.secondHalfyearly.push(calenderModel1);
       this.prepareQuaterlyObj();
 
-    } else if (val == 8) {
+    } else if (val === 8) {
       this.getDateTimeEvent(val);
       this.julDate = null;
       this.augDate = null;
@@ -270,13 +270,13 @@ export class HalfyearlyComponent implements OnInit {
 
       this.secondHalfyearly = [];
       let calenderModel1 = new CalenderModel();
-      var jan = this.datePipe.transform(this.sepDate, 'yyyy-MM-dd');
+      let jan = this.datePipe.transform(this.sepDate, 'yyyy-MM-dd');
       calenderModel1.start_date = jan;
       calenderModel1.end_date = jan;
       this.secondHalfyearly.push(calenderModel1);
       this.prepareQuaterlyObj();
 
-    } else if (val == 9) {
+    } else if (val === 9) {
       this.getDateTimeEvent(val);
       this.julDate = null;
       this.augDate = null;
@@ -286,13 +286,13 @@ export class HalfyearlyComponent implements OnInit {
 
       this.secondHalfyearly = [];
       let calenderModel1 = new CalenderModel();
-      var jan = this.datePipe.transform(this.octDate, 'yyyy-MM-dd');
+      let jan = this.datePipe.transform(this.octDate, 'yyyy-MM-dd');
       calenderModel1.start_date = jan;
       calenderModel1.end_date = jan;
       this.secondHalfyearly.push(calenderModel1);
       this.prepareQuaterlyObj();
 
-    } else if (val == 10) {
+    } else if (val === 10) {
       this.getDateTimeEvent(val);
       this.julDate = null;
       this.augDate = null;
@@ -302,13 +302,13 @@ export class HalfyearlyComponent implements OnInit {
 
       this.secondHalfyearly = [];
       let calenderModel1 = new CalenderModel();
-      var jan = this.datePipe.transform(this.novDate, 'yyyy-MM-dd');
+      let jan = this.datePipe.transform(this.novDate, 'yyyy-MM-dd');
       calenderModel1.start_date = jan;
       calenderModel1.end_date = jan;
       this.secondHalfyearly.push(calenderModel1);
       this.prepareQuaterlyObj();
 
-    } else if (val == 11) {
+    } else if (val === 11) {
       this.getDateTimeEvent(val);
       this.julDate = null;
       this.augDate = null;
@@ -318,7 +318,7 @@ export class HalfyearlyComponent implements OnInit {
 
       this.secondHalfyearly = [];
       let calenderModel1 = new CalenderModel();
-      var jan = this.datePipe.transform(this.decDate, 'yyyy-MM-dd');
+      let jan = this.datePipe.transform(this.decDate, 'yyyy-MM-dd');
       calenderModel1.start_date = jan;
       calenderModel1.end_date = jan;
       this.secondHalfyearly.push(calenderModel1);
--- src/app/quarterly/quarterly.component.ts
+++ src/app/quarterly/quarterly.component.ts
@@ -13,11 +13,11 @@ import { DatePipe } from '@angular/common';
   styleUrls: ['./quarterly.component.css']
 })
 export class QuarterlyComponent implements OnInit {
-  selectedCheckBox: number = 0;
-  firstCheckBox: boolean = true;
-  secondCheckBox: boolean = true;
-  thirdCheckBox: boolean = true;
-  fourCheckBox: boolean = true;
+  selectedCheckBox = 0;
+  firstCheckBox = true;
+  secondCheckBox = true;
+  thirdCheckBox = true;
+  fourCheckBox = true;
   quarterlyModelArray: QuarterlyModel = <QuarterlyModel>{};
 
 
@@ -44,10 +44,9 @@ export class QuarterlyComponent implements OnInit {
   public decDate: Date;
 
   public selectYear: any;
-
   public startYear: string;
   public year: string;
-  public mainErrorMsg ='';
+  public mainErrorMsg = '';
   public dateValidationFlag = false;
 
   firstQuart: Array<CalenderModel> = [];
@@ -58,15 +57,9 @@ export class QuarterlyComponent implements OnInit {
   validationModel: ValidationModel = new ValidationModel();
 
   utilsObj: Utils = new Utils(this.datePipe);
-
-
-  public startAt = new Date(2018, 3, 18, 10, 30);
-
-  // Min moment: April 12 2018, 10:30
-  public min = new Date(2018, 3, 12, 10, 30);
-
-  // Max moment: April 25 2018, 20:30
-  public max = new Date(2018, 3, 25, 20, 30);
+  public startAt = null;
+  public min = null;
+  public max = null;
   constructor(public datePipe: DatePipe) {
     this.quarterlyModelArray.quarterlyModel = [];
   }
@@ -80,37 +73,28 @@ export class QuarterlyComponent implements OnInit {
   handleSelectYear(year: string) {
     this.year = year;
     this.startYear = year;
+    this.selectYear = year;
     this.dateValidationFlag = false;
-    /*  this.firstCheckBox = false;
-     this.secondCheckBox = false;
-     this.thirdCheckBox = false;
-     this.fourCheckBox = false; */
-    //console.log("Kiran--->" + this.startYear);
   }
 
   checkBoxChangeEvent(event) {
-    //console.log("2--->" + this.startYear);
-    if (this.startYear == undefined) {
-      this.mainErrorMsg = " Please select Year";
+    if (this.startYear === undefined) {
+      this.mainErrorMsg = 'Please select Year';
       this.dateValidationFlag = true;
       this.firstCheckBox = false;
       this.secondCheckBox = false;
       this.thirdCheckBox = false;
       this.fourCheckBox = false;
     } else {
-
-
-      console.log("value-->" + val);
+     // console.log('value-->' + val);
       this.dateValidationFlag = false;
     }
 
-    var val = event.target.value;
-    var index = this.selectOption.findIndex((ds: any) => {
-      return ds.id + "" === event.target.id;
+    let val = event.target.value;
+    let index = this.selectOption.findIndex((ds: any) => {
+      return ds.id + '' === event.target.id;
     });
     this.selectedCheckBox = index;
-    //console.log(val);
-    // console.log(event.target.checked);
     if (val === '1' && event.target.checked) {
       this.firstCheckBox = false;
     } else if (val === '1' && !event.target.checked) {
@@ -126,7 +110,6 @@ export class QuarterlyComponent implements OnInit {
 
     } else if (val === '2' && !event.target.checked) {
       this.secondCheckBox = true;
-      // this.secondQuart = [];
     }
 
     if (val === '3' && event.target.checked) {
@@ -134,7 +117,6 @@ export class QuarterlyComponent implements OnInit {
 
     } else if (val === '3' && !event.target.checked) {
       this.thirdCheckBox = true;
-      // this.thirdQuart = [];
     }
 
     if (val === '4' && event.target.checked) {
@@ -142,7 +124,6 @@ export class QuarterlyComponent implements OnInit {
 
     } else if (val === '4' && !event.target.checked) {
       this.fourCheckBox = true;
-      //this.fourthQuart = [];
     }
   }
   selectedOptions() { // right now: ['1','3']
@@ -152,23 +133,21 @@ export class QuarterlyComponent implements OnInit {
   }
 
   public getMonthFirstLastDay(year, month): any {
-    var monthStartDay = new Date(year, month, 1);
-    var monthEndDay = new Date(year, month + 1, 0);
-    var min = this.utilsObj.formatDate(monthStartDay, 'dd');
-    var max = this.utilsObj.formatDate(monthEndDay, 'dd');
-
+    let monthStartDay = new Date(year, month, 1);
+    let monthEndDay = new Date(year, month + 1, 0);
+    let min = this.utilsObj.formatDate(monthStartDay, 'dd');
+    let max = this.utilsObj.formatDate(monthEndDay, 'dd');
     this.min = new Date(this.selectYear, month, Number.parseInt(min));
     this.max = new Date(this.selectYear, month, Number.parseInt(max));
 
   }
 
   getDateTimeEvent(val) {
-    this.selectYear = this.startAt.getFullYear();
+    // this.selectYear = this.startAt.getFullYear();
     this.getMonthFirstLastDay(this.selectYear, val);
   }
 
   handleChange(event) {
-    //console.log("handle change");
     this.dateChanged.emit(this.quarterlyModelArray);
   }
 
@@ -199,132 +178,120 @@ export class QuarterlyComponent implements OnInit {
     if (this.fourthQuart.length > 0) {
       this.quarterlyModelArray.quarterlyModel.push(this.fourthQuart[0]);
     }
-    //console.log("quarterlyModelArray" + JSON.stringify(this.quarterlyModelArray));
+    // console.log("quarterlyModelArray" + JSON.stringify(this.quarterlyModelArray));
     this.handleChange(event);
   }
 
   clickMonthEvent(val) {
-    if (val == 0) {
+    if (val === 0) {
       this.getDateTimeEvent(val);
       this.febDate = null;
       this.marchDate = null;
       this.firstQuart = [];
 
       let calenderModel1 = new CalenderModel();
-      var jan = this.datePipe.transform(this.janDate, 'yyyy-MM-dd');
+      let jan = this.datePipe.transform(this.janDate, 'yyyy-MM-dd');
       calenderModel1.start_date = jan;
       calenderModel1.end_date = jan;
       this.firstQuart.push(calenderModel1);
-
       this.prepareQuaterlyObj();
-
-    } else if (val == 1) {
+    } else if (val === 1) {
       this.getDateTimeEvent(val);
       this.janDate = null;
       this.marchDate = null;
       this.firstQuart = [];
 
       let calenderModel1 = new CalenderModel();
-      var jan = this.datePipe.transform(this.febDate, 'yyyy-MM-dd');
+      let jan = this.datePipe.transform(this.febDate, 'yyyy-MM-dd');
       calenderModel1.start_date = jan;
       calenderModel1.end_date = jan;
-
       this.firstQuart.push(calenderModel1);
-
       this.prepareQuaterlyObj();
-    } else if (val == 2) {
+    } else if (val === 2) {
       this.getDateTimeEvent(val);
       this.febDate = null;
       this.janDate = null;
       this.firstQuart = [];
 
       let calenderModel1 = new CalenderModel();
-      var jan = this.datePipe.transform(this.marchDate, 'yyyy-MM-dd');
+      let jan = this.datePipe.transform(this.marchDate, 'yyyy-MM-dd');
       calenderModel1.start_date = jan;
       calenderModel1.end_date = jan;
       this.firstQuart.push(calenderModel1);
-
       this.prepareQuaterlyObj();
-    } else if (val == 3) {
+    } else if (val === 3) {
       this.getDateTimeEvent(val);
       this.mayDate = null;
       this.juneDate = null;
       this.secondQuart = [];
-
       let calenderModel1 = new CalenderModel();
-      var apr = this.datePipe.transform(this.aprDate, 'yyyy-MM-dd');
+      let apr = this.datePipe.transform(this.aprDate, 'yyyy-MM-dd');
       calenderModel1.start_date = apr;
       calenderModel1.end_date = apr;
       this.secondQuart.push(calenderModel1);
-
       this.prepareQuaterlyObj();
-    } else if (val == 4) {
+    } else if (val === 4) {
       this.getDateTimeEvent(val);
       this.aprDate = null;
       this.juneDate = null;
       this.secondQuart = [];
       let calenderModel1 = new CalenderModel();
-      var apr = this.datePipe.transform(this.mayDate, 'yyyy-MM-dd');
+      let apr = this.datePipe.transform(this.mayDate, 'yyyy-MM-dd');
       calenderModel1.start_date = apr;
       calenderModel1.end_date = apr;
       this.secondQuart.push(calenderModel1);
-
       this.prepareQuaterlyObj();
-    } else if (val == 5) {
+    } else if (val === 5) {
       this.getDateTimeEvent(val);
       this.mayDate = null;
       this.aprDate = null;
       this.secondQuart = [];
       let calenderModel1 = new CalenderModel();
-      var apr = this.datePipe.transform(this.juneDate, 'yyyy-MM-dd');
+      let apr = this.datePipe.transform(this.juneDate, 'yyyy-MM-dd');
       calenderModel1.start_date = apr;
       calenderModel1.end_date = apr;
       this.secondQuart.push(calenderModel1);
-
       this.prepareQuaterlyObj();
-    } else if (val == 6) {
+    } else if (val === 6) {
       this.getDateTimeEvent(val);
       this.augDate = null;
       this.sepDate = null;
-
       this.thirdQuart = [];
       let calenderModel1 = new CalenderModel();
-      var apr = this.datePipe.transform(this.julDate, 'yyyy-MM-dd');
+      let apr = this.datePipe.transform(this.julDate, 'yyyy-MM-dd');
       calenderModel1.start_date = apr;
       calenderModel1.end_date = apr;
       this.thirdQuart.push(calenderModel1);
       this.prepareQuaterlyObj();
-
-    } else if (val == 7) {
+    } else if (val === 7) {
       this.getDateTimeEvent(val);
       this.julDate = null;
       this.sepDate = null;
       this.thirdQuart = [];
       let calenderModel1 = new CalenderModel();
-      var apr = this.datePipe.transform(this.augDate, 'yyyy-MM-dd');
+      let apr = this.datePipe.transform(this.augDate, 'yyyy-MM-dd');
       calenderModel1.start_date = apr;
       calenderModel1.end_date = apr;
       this.thirdQuart.push(calenderModel1);
-
       this.prepareQuaterlyObj();
-    } else if (val == 8) {
+    } else if (val === 8) {
       this.getDateTimeEvent(val);
       this.augDate = null;
       this.julDate = null;
       this.thirdQuart = [];
       let calenderModel1 = new CalenderModel();
-      var apr = this.datePipe.transform(this.sepDate, 'yyyy-MM-dd');
+      let apr = this.datePipe.transform(this.sepDate, 'yyyy-MM-dd');
       calenderModel1.start_date = apr;
       calenderModel1.end_date = apr;
       this.thirdQuart.push(calenderModel1);
       this.prepareQuaterlyObj();
-    } else if (val == 9) {
+    } else if (val === 9) {
       this.getDateTimeEvent(val);
       this.decDate = null;
       this.novDate = null;
       this.fourthQuart = [];
       let calenderModel1 = new CalenderModel();
-      var apr = this.datePipe.transform(this.octDate, 'yyyy-MM-dd');
+      let apr = this.datePipe.transform(this.octDate, 'yyyy-MM-dd');
       calenderModel1.start_date = apr;
       calenderModel1.end_date = apr;
       this.fourthQuart.push(calenderModel1);
@@ -335,7 +302,7 @@ export class QuarterlyComponent implements OnInit {
       this.decDate = null;
       this.fourthQuart = [];
       let calenderModel1 = new CalenderModel();
-      var apr = this.datePipe.transform(this.novDate, 'yyyy-MM-dd');
+      let apr = this.datePipe.transform(this.novDate, 'yyyy-MM-dd');
       calenderModel1.start_date = apr;
       calenderModel1.end_date = apr;
       this.fourthQuart.push(calenderModel1);
@@ -346,7 +313,7 @@ export class QuarterlyComponent implements OnInit {
       this.octDate = null;
       this.thirdQuart = [];
       let calenderModel1 = new CalenderModel();
-      var apr = this.datePipe.transform(this.decDate, 'yyyy-MM-dd');
+      let apr = this.datePipe.transform(this.decDate, 'yyyy-MM-dd');
       calenderModel1.start_date = apr;
       calenderModel1.end_date = apr;
       this.fourthQuart.push(calenderModel1);
