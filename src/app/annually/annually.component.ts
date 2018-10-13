7c7073cc25595ec2bd6235de1adcb32c98a92643
kiran5506 <chinthadakirankumar@gmail.com>
2018-10-13 11:40:04 +0530

    last commit of anullay

--- src/app/annually/annually.component.ts
+++ src/app/annually/annually.component.ts
@@ -1,47 +1,15 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import {
-  DateTimeAdapter,
-  OWL_DATE_TIME_FORMATS,
-  OWL_DATE_TIME_LOCALE,
-  OwlDateTimeComponent,
-  OwlDateTimeFormats
-} from 'ng-pick-datetime';
-import { MomentDateTimeAdapter } from 'ng-pick-datetime-moment';
 import * as _moment from 'moment';
-import { Moment } from 'moment';
 import { CalenderModel } from '../models/calender';
 const moment = (_moment as any).default ? (_moment as any).default : _moment;
-
-export const MY_MOMENT_DATE_TIME_FORMATS: OwlDateTimeFormats = {
-  parseInput: 'MM/YYYY',
-  fullPickerInput: 'l LT',
-  datePickerInput: 'MM/YYYY',
-  timePickerInput: 'LT',
-  monthYearLabel: 'MMM YYYY',
-  dateA11yLabel: 'LL',
-  monthYearA11yLabel: 'MMMM YYYY',
-};
-
 
 @Component({
   selector: 'app-annually',
   templateUrl: './annually.component.html',
-  styleUrls: ['./annually.component.css'],
-  providers: [
-    // `MomentDateTimeAdapter` and `OWL_MOMENT_DATE_TIME_FORMATS` can be automatically provided by importing
-    // `OwlMomentDateTimeModule` in your applications root module. We provide it at the component level
-    // here, due to limitations of our example generation script.
-    { provide: DateTimeAdapter, useClass: MomentDateTimeAdapter, deps: [OWL_DATE_TIME_LOCALE] },
-
-    { provide: OWL_DATE_TIME_FORMATS, useValue: MY_MOMENT_DATE_TIME_FORMATS },
-  ],
+  styleUrls: ['./annually.component.css']
 })
 export class AnnuallyComponent implements OnInit {
 
-  public selectedMoment: string;
-  public startDate: Date;
-  public endDate: Date;
   public selectDate: Date = null;
   public annuallyCalendarObj: Array<CalenderModel> = [];
   public endYearValidationFlag = true;
@@ -67,8 +35,6 @@ export class AnnuallyComponent implements OnInit {
 
   handleSelectYear(year: string, id: number) {
     this.year = year;
-    console.log('tttt' + this.year);
-    console.log('id' + id);
     if (id === 0) {
       this.startYear = parseInt(year, 10);
       this.startYearHandler(event);
@@ -84,7 +50,6 @@ export class AnnuallyComponent implements OnInit {
     if (this.startYear === undefined) {
       this.endYearValidationFlag = true;
     } else {
-      console.log('start Year', this.startYear);
       if (this.endYear !== undefined) {
         if (this.endYear <= this.startYear) {
           this.startYearConditionValidationFlag = false;
@@ -127,30 +92,22 @@ export class AnnuallyComponent implements OnInit {
 
     if (this.startYear === undefined && this.endYear === undefined) {
     } else if (this.selectDate !== null) {
-
-      console.log('selectDate ---', this.selectDate);
       let currentDate: Date = new Date(moment(this.selectDate).format('YYYY-MM-DD'));
-
       let currentYear = currentDate.getFullYear();
       this.annuallyCalendarObj = [];
       if ((this.startYear <= currentYear) && (this.endYear >= currentYear)) {
         this.startEndYearBetweenValidationFlag = true;
         for (let i = this.startYear; i <= this.endYear; i++) {
-          // console.log('selectDateHandler' + this.startYear + "End Year" + this.endYear);
           let calDate = currentDate;
           let month = calDate.getMonth();
           let date = calDate.getDate();
           let selectedDateObj = new Date(i, month, date);
-          console.log("selectedDateObj" + selectedDateObj);
           let dayObj = moment(selectedDateObj).format('YYYY-MM-DD');
           let calenderModel = new CalenderModel();
           calenderModel.start_date = dayObj;
           calenderModel.end_date = dayObj;
           this.annuallyCalendarObj.push(calenderModel);
-          //if (i === this.endYear) {
           this.annuallyCalendarChanged.emit(this.annuallyCalendarObj)
-          console.log("final annually object--->" + JSON.stringify(this.annuallyCalendarObj));
-          //}
         }
       } else {
         this.startEndYearBetweenValidationFlag = false;
